211756751d562874d14d647188eca601bf70e986
Simone Arena <122620587+WasamiKirua@users.noreply.github.com>
2024-07-20 11:44:44 +0200

    Update preparation.py

--- preparation.py
+++ preparation.py
@@ -20,7 +20,7 @@ import json
 VASTAI_KEY = ""
 YAML_FILE = "fft-1.6b.yaml"
 YAML_PATH = '/workspace/axolotl/examples/stablelm-2/1.6b'
-
+TARGET_TAG = 'axolotl-stablelm'
 
 print(f"{Fore.GREEN}Setting Vast.ai api key{Style.RESET_ALL}\n")
 os.system(f"vastai set api-key {VASTAI_KEY}")
@@ -33,35 +33,45 @@ print()
 print(f"{Fore.GREEN}Instance infos:{Style.RESET_ALL}\n")
 with open("infos.json", "r") as f:
     data = json.load(f)
+
+    # Extract the desired fields
+    extracted_data = []
     for instance in data:
-        instance_details = {
+        ssh_port = instance["ports"]['22/tcp'][0]['HostPort']
+        instance_info = {
             "instance_id": instance["id"],
             "public_ip": instance["public_ipaddr"],
             "ssh_host": instance["ssh_host"],
-            "ssh_port": instance["ssh_port"]
+            "ssh_port": ssh_port,
+            "label": instance["label"],
+            "-----": "-----"
         }
+        extracted_data.append(instance_info)
 
-        # Print each key-value pair for the current instance
-        for key, value in instance_details.items():
-            print(f"{Fore.GREEN}{key.replace('_', ' ').capitalize()}: {value}{Style.RESET_ALL}")
+for instance in extracted_data:
+    # Print each key-value pair for the current instance
+    for key, value in instance.items():
+        print(f"{Fore.GREEN}{key.replace('_', ' ').capitalize()}: {value}{Style.RESET_ALL}")
         print()
-
-print(f"{Fore.CYAN}Copying datasets into /workspace{Style.RESET_ALL}\n")
-os.system(f"scp -P {instance_details['ssh_port']} -o StrictHostKeyChecking=accept-new datasets/*.jsonl root@{instance_details['ssh_host']}:/workspace")
-print()
-
-print(f"{Fore.CYAN}Copying configuration file into axolotl example's folder{Style.RESET_ALL}\n")
-os.system(f"scp -P {instance_details['ssh_port']} -o StrictHostKeyChecking=accept-new {YAML_FILE} root@{instance_details['ssh_host']}:{YAML_PATH}")
-print()
-
-print(f"{Fore.CYAN}Copying py training script{Style.RESET_ALL}\n")
-os.system(f"scp -P {instance_details['ssh_port']} -o StrictHostKeyChecking=accept-new run_training.py root@{instance_details['ssh_host']}:/workspace/axolotl")
-print()
-
-print(f"{Fore.GREEN}Done !{Style.RESET_ALL}\n")
-sleep(3)
-print()
-
-print(f"{Fore.CYAN}Connecting to the instance...{Style.RESET_ALL}\n")
-os.system(f"ssh -p {instance_details['ssh_port']} -o StrictHostKeyChecking=accept-new root@{instance_details['ssh_host']}")
-print()
+        if key == "label" and value == TARGET_TAG:
+            print("************")
+            print(f"{Fore.CYAN}Found {Fore.RED}{instance['label']}{Style.RESET_ALL} target instance !{Style.RESET_ALL}\n")
+            print("************")
+            print(f"{Fore.CYAN}Copying datasets to instance {instance['label']}{Style.RESET_ALL}\n")
+            os.system(f"scp -P {instance['ssh_port']} -o StrictHostKeyChecking=accept-new datasets/*.jsonl root@{instance['public_ip']}:/workspace")
+            print()
+            print(f"{Fore.CYAN}Copying yaml conf to instance {instance['label']}{Style.RESET_ALL}\n")
+            os.system(f"scp -P {instance['ssh_port']} -o StrictHostKeyChecking=accept-new {YAML_FILE} root@{instance['public_ip']}:{YAML_PATH}")
+            print()
+            print(f"{Fore.CYAN}Copying training script to instance {instance['label']}{Style.RESET_ALL}\n")
+            os.system(f"scp -P {instance['ssh_port']} -o StrictHostKeyChecking=accept-new run_training.py root@{instance['public_ip']}:/workspace/axolotl")
+            print()
+            print(f"{Fore.CYAN}Copying JSON to instance {instance['label']}{Style.RESET_ALL}\n")
+            os.system(f"scp -P {instance['ssh_port']} -o StrictHostKeyChecking=accept-new infos.json root@{instance['public_ip']}:/workspace/axolotl")
+            print()
+            print(f"{Fore.CYAN}Copying deps.sh to instance {instance['label']}{Style.RESET_ALL}\n")
+            os.system(f"scp -P {instance['ssh_port']} -o StrictHostKeyChecking=accept-new deps.sh root@{instance['public_ip']}:/workspace/axolotl")
+            print()
+            print(f"{Fore.CYAN}Connecting to the instance...{Style.RESET_ALL}\n")
+            os.system(f"ssh -p {instance['ssh_port']} -o StrictHostKeyChecking=accept-new root@{instance['public_ip']}")
+            print()
